d72cb55052ca4c0b0e1222c74e009dd80ac667e9
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-03-18 14:06:56 +0530

    Stage 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,17 @@
-// Learn more at developers.reddit.com/docs
 import { Devvit, useState } from '@devvit/public-api';
 
-Devvit.configure({
-  redditAPI: true,
-});
-
-// Add a menu item to the subreddit menu for instantiating the new experience post
-Devvit.addMenuItem({
-  label: 'Add my post',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async (_event, context) => {
-    const { reddit, ui } = context;
-    ui.showToast("Submitting your post - upon completion you'll navigate there.");
-
-    const subreddit = await reddit.getCurrentSubreddit();
-    const post = await reddit.submitPost({
-      title: 'My devvit post',
-      subredditName: subreddit.name,
-      // The preview appears while the post loads
-      preview: (
-        <vstack height="100%" width="100%" alignment="middle center">
-          <text size="large">Loading ...</text>
-        </vstack>
-      ),
-    });
-    ui.navigateTo(post);
-  },
-});
-
-// Add a post type definition
 Devvit.addCustomPostType({
-  name: 'Experience Post',
-  height: 'regular',
-  render: (_context) => {
+  name: 'Caption This!',
+  render: (context) => {
     const [counter, setCounter] = useState(0);
-
     return (
-      <vstack height="100%" width="100%" gap="medium" alignment="center middle">
-        <image
-          url="meme.jpg"
-          description="meme"
-          imageHeight={500}
-          imageWidth={500}
-          height="500px"
-          width="500px"
-        />
-        <text size="large">Enter Caption!</text>
+      <vstack alignment="center middle" height="100%" gap="large">
+        <text size="xxlarge" weight="bold">Enter Caption For This Meme!</text>
+
+        
+        
         <button appearance="primary">Submit!</button>
+        {counter ? <text>{`You clicked ${counter} time(s)!`}</text> : <text>&nbsp;</text>}
       </vstack>
     );
   },
